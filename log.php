d69d413c7db0e4324d97584bc919927afffa609a
KrutiA <krutiaparnathi2010@gmail.com>
2019-03-05 12:50:37 +0530

    Create log.php

---
+++ log.php
@@ -0,0 +1,6 @@
+<?php
+$writer = new \Zend\Log\Writer\Stream(BP . '/var/log/test.log');
+            $logger = new \Zend\Log\Logger();
+            $logger->addWriter($writer);
+            $logger->info('Call CustomerwaitingNotification: sent Mail'.rand());
+            ?>
